93feac4aa65e3897fe4c45eb3cf51b50d56d6658
Rick Clephas <rick.clephas.external@jetbrains.com>
2026-08-11 11:59:26 +0200

    Use casts for function overloads

--- kotlin-swift-interopedia-samples-swift/iosApp/iosApp/MoreAboutFunctions/FunctionsWithOverloadsExample.swift
+++ kotlin-swift-interopedia-samples-swift/iosApp/iosApp/MoreAboutFunctions/FunctionsWithOverloadsExample.swift
@@ -3,9 +3,9 @@ import shared
 
 func functionsWithOverloadsExample1() {
     moreaboutfunctions.overloadFunction(param: true)  // Bool
-    moreaboutfunctions.overloadFunction(param_: 2.0)  // Double
-    moreaboutfunctions.overloadFunction(param__: 2.0) // Float
-    moreaboutfunctions.overloadFunction(param___: 2)  // Int32
-    moreaboutfunctions.overloadFunction(param____: 4) // Int64
-    moreaboutfunctions.overloadFunction(param_____: "123")
+    moreaboutfunctions.overloadFunction(param: 2.0 as Double)  // Double
+    moreaboutfunctions.overloadFunction(param: 2.0 as Float) // Float
+    moreaboutfunctions.overloadFunction(param: 2 as Int32)  // Int32
+    moreaboutfunctions.overloadFunction(param: 4 as Int64) // Int64
+    moreaboutfunctions.overloadFunction(param: "123")
 }
