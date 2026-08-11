6d3667e65c0650a071dc5f09f8ec8eb6de8859d5
Rick Clephas <rick.clephas.external@jetbrains.com>
2026-08-11 11:53:37 +0200

    Replace KotlinNothing with Never

--- kotlin-swift-interopedia-samples-swift/iosApp/iosApp/Types/UnitNothingExample.swift
+++ kotlin-swift-interopedia-samples-swift/iosApp/iosApp/Types/UnitNothingExample.swift
@@ -5,7 +5,7 @@ func unitNothingExample(){
     let example = types.UnitNothing()
     example.unitType(p: ())
     //Doesn't compile - no init() function
-    //example.nothingType(n: KotlinNothing())
+    //example.nothingType(n: Never())
     print(example.returnUnit())
     //Crashes app - exception
     //example.returnNothing()
