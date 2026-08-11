67514f5cb443dfad807bec984fe84efab96f2e6a
Rick Clephas <rick.clephas.external@jetbrains.com>
2026-08-11 11:39:56 +0200

    Add `outer__` argument label to inner class init

--- kotlin-swift-interopedia-samples-swift/iosApp/iosApp/Classes/InnerClassesExample.swift
+++ kotlin-swift-interopedia-samples-swift/iosApp/iosApp/Classes/InnerClassesExample.swift
@@ -2,7 +2,7 @@ import Foundation
 import shared
 
 func innerClassExample() {
-    let innerClass = classesandinterfaces.OuterClass.InnerClass(classesandinterfaces.OuterClass(param: "1323"))
+    let innerClass = classesandinterfaces.OuterClass.InnerClass(outer__: classesandinterfaces.OuterClass(param: "1323"))
     print(innerClass.useSomeFunction())
 }
 
