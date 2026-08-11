7b8afc69e3fde17417ca105476ba26ae4bde6134
Rick Clephas <rick.clephas.external@jetbrains.com>
2026-08-11 14:04:38 +0200

    Generic class init requires value of type T

--- kotlin-swift-interopedia-samples-swift/iosApp/iosApp/Generics/BoundedGenericsExample.swift
+++ kotlin-swift-interopedia-samples-swift/iosApp/iosApp/Generics/BoundedGenericsExample.swift
@@ -6,6 +6,6 @@ func boundedGenericsExample(){
 
     let result1 = generics.StrictedGeneric(data: MyStricted())
     print(result1)
-    let result2 = generics.StrictedGeneric(data: NSString("1122")) // Compiles
-    print(result2)
+//    let result2 = generics.StrictedGeneric(data: NSString("1122")) // Does not compile
+//    print(result2)
 }
