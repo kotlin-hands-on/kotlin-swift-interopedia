7eeba88db84376e4de165756b77b264808e47392
Rick Clephas <rick.clephas.external@jetbrains.com>
2026-08-11 13:47:37 +0200

    Suspend functions can't be called with a completionHandler

--- kotlin-swift-interopedia-samples-swift/iosApp/iosApp/Coroutines/SuspendFunctionCompletionHandlerExample.swift
+++ kotlin-swift-interopedia-samples-swift/iosApp/iosApp/Coroutines/SuspendFunctionCompletionHandlerExample.swift
@@ -2,7 +2,8 @@ import Foundation
 import shared
 
 func suspendFunctionCompletionHandlerExample() {
-    coroutines.ThingRepository().getThingSimple(succeed: true, completionHandler: { thing, error in
-        print("Thing is \(thing). Error is \(error)")
-    })
+//    suspend function can't be called with a completionHandler in Swift Export
+//    coroutines.ThingRepository().getThingSimple(succeed: true, completionHandler: { thing, error in
+//        print("Thing is \(thing). Error is \(error)")
+//    })
 }
